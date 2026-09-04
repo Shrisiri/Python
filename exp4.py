79b9c1c5737e0d7155ba873dc081df0fda540ac2
Shrisiri <shrisiri.25004579@jainuniversity.ac.in>
2026-09-04 11:27:33 +0530

    Add files via upload

---
+++ exp4.py
@@ -0,0 +1,51 @@
+numbers = [50,20,30,10,40,20]
+print("Original List:",numbers)
+
+#length
+print("Length of list",len(numbers))
+
+#append
+numbers.append(60)
+print("After append:",numbers)
+
+#insert
+numbers.insert(2,25)
+print("After insert(2,25)",numbers)
+
+#remove
+numbers.remove(20)
+print("After removing:",numbers)
+
+#sort
+numbers.sort()
+print("After sort:",numbers)
+
+#reverse
+numbers.reverse()
+print("After reversal:",numbers)
+
+#copy
+new_list = numbers.copy()
+print("After copy",new_list)
+
+#max
+print("Max is:",max(numbers))
+
+#min
+print("Min is:",min(numbers))
+
+
+#sum
+print("Total sum is:",sum(numbers))
+
+
+#extend
+numbers.extend([30,20])
+print("After extend",numbers)
+
+#pop
+top_ele = numbers.pop()
+print("After pop():",numbers)
+print("Popped item:",top_ele)
+
+
